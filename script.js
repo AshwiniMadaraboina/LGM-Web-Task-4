c33a3d1c6a8b944467ca18021dfe30e961a6442e
ASHWINIMADARABOINA <ashwinimadaraboina@gmail.com>
2023-06-28 18:19:39 +0530

    second commit

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let string="";
 let buttons=document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
+        
         if(e.target.innerHTML=='='){
+            try{
             string=eval(string);
             document.querySelector('input').value=string;
         }
+        catch(error){
+            document.querySelector('input').value="invalid format";
+        }}
         else if(e.target.innerHTML=='C'){
             string="";
             document.querySelector('input').value=string;
